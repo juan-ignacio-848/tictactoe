afe486ea7bcb86be8bd5eb3724473744f1e1cdc8
nmkip <juan.ignacio848@gmail.com>
2019-02-21 23:15:44 -0300

    Extracted method and re-arranged code.

--- src/main/java/com/nmkip/tictactoe/Board.java
+++ src/main/java/com/nmkip/tictactoe/Board.java
@@ -24,12 +24,24 @@ class Board {
         return takenSquaresByPlayer.keySet().contains(square);
     }
 
+    Board take(Square square, Player player) {
+        Map<Square, Player> squares = new HashMap<>(takenSquaresByPlayer);
+        squares.put(square, player);
+        return new Board(squares);
+    }
+
     boolean hasDrawCombination() {
         return takenSquaresByPlayer.size() == 9;
     }
 
     boolean hasWinningCombination(Player player) {
-        Stream<Stream<Square>> winningCombinations = Stream.of(
+        return winningCombinations().anyMatch(winningCombination -> winningCombination.allMatch(
+                takenSquaresBy(player)::contains
+        ));
+    }
+
+    private Stream<Stream<Square>> winningCombinations() {
+        return Stream.of(
                 Stream.of(TOP_LEFT, TOP_MIDDLE, TOP_RIGHT),
                 Stream.of(CENTER_LEFT, CENTER_MIDDLE, CENTER_RIGHT),
                 Stream.of(BOTTOM_LEFT, BOTTOM_MIDDLE, BOTTOM_RIGHT),
@@ -39,10 +51,6 @@ class Board {
                 Stream.of(TOP_LEFT, CENTER_MIDDLE, BOTTOM_RIGHT),
                 Stream.of(TOP_RIGHT, CENTER_MIDDLE, BOTTOM_LEFT)
         );
-
-        return winningCombinations.anyMatch(winningCombination -> winningCombination.allMatch(
-                takenSquaresBy(player)::contains
-        ));
     }
 
     private Set<Square> takenSquaresBy(Player player) {
@@ -50,11 +58,5 @@ class Board {
                                    .filter(entry -> entry.getValue() == player)
                                    .map(Map.Entry::getKey)
                                    .collect(Collectors.toSet());
-    }
-
-    Board take(Square square, Player player) {
-        Map<Square, Player> squares = new HashMap<>(takenSquaresByPlayer);
-        squares.put(square, player);
-        return new Board(squares);
     }
 }
