53dd55631f002fa3c90b3b7fae9cd24c0c06de26
nmkip <juan.ignacio848@gmail.com>
2019-02-21 21:02:41 -0300

    Refactored tests.

--- src/test/java/com/nmkip/tictactoe/GameShould.java
+++ src/test/java/com/nmkip/tictactoe/GameShould.java
@@ -3,9 +3,12 @@ package com.nmkip.tictactoe;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static com.nmkip.tictactoe.Player.*;
 import static com.nmkip.tictactoe.Square.*;
 import static com.nmkip.tictactoe.Status.*;
+import static java.util.Arrays.*;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -21,41 +24,35 @@ class GameShould {
 
     @Test
     void wait_for_O_to_play_after_X() {
-        Game game = new Game();
-
-        game = game.play(TOP_LEFT);
+        Game game = play(TOP_LEFT);
 
         assertThat(game.state(), is(new GameState(GAME_ON, O)));
     }
 
     @Test
     void alternate_players() {
-        Game game = new Game();
-
-        game = game.play(TOP_LEFT);
-        game = game.play(TOP_RIGHT);
+        Game game = play(TOP_LEFT, TOP_RIGHT);
 
         assertThat(game.state(), is(new GameState(GAME_ON, X)));
     }
 
     @Test
     void not_allow_different_players_to_play_on_a_played_square() {
-        Game game = new Game();
-
-        game = game.play(TOP_LEFT);
-        game = game.play(TOP_LEFT);
+        Game game = play(TOP_LEFT, TOP_LEFT);
 
         assertThat(game.state(), is(new GameState(GAME_ON, O)));
     }
 
     @Test
     void not_allow_the_same_player_to_play_on_a_played_square() {
-        Game game = new Game();
-
-        game = game.play(TOP_LEFT);
-        game = game.play(TOP_RIGHT);
-        game = game.play(TOP_LEFT);
+        Game game = play(TOP_LEFT, TOP_RIGHT, TOP_LEFT);
 
         assertThat(game.state(), is(new GameState(GAME_ON, X)));
     }
+
+    private Game play(Square... squares) {
+        return stream(squares)
+                .reduce(new Game(), Game::play, (game, game2) -> game);
+    }
+
 }
